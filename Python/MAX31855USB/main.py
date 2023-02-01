7b7ebfcc0f721abcd373a27ce76e3d8e4851385f
Friedrich Kiesel <57568035+kiesel-electronics@users.noreply.github.com>
2023-02-01 22:52:19 +0100

    Added pictures

--- Python/MAX31855USB/main.py
+++ Python/MAX31855USB/main.py
@@ -7,10 +7,10 @@ print("Installed pymeasure version: " + pymeasure.__version__)
 print("Available resources:")
 print(list_resources())
 
-tc = MAX31855USB('ASRL14')
+tc = MAX31855USB('ASRL13')
 print("Device id: " + tc.id())
 
-for i in range(5):
+for i in range(500):
     print("Measured temperature (°C):          " + str(tc.get_temperature()))
     print("Measured internal temperature (°C): " + str(tc.get_internal_temperature()))
     time.sleep(1)
